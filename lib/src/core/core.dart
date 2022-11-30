4118d5aa79ceb5962034c27d92d79ccee5805aca
Mahmoud Khalid <darkness20016@gmail.com>
2022-11-30 22:27:20 +0200

    Rename metadata file to data_handler

--- lib/src/core/core.dart
+++ lib/src/core/core.dart
@@ -1,7 +1,7 @@
 export 'constants.dart';
 export 'sizepack.dart';
 export 'path_handler.dart';
-export 'metadata.dart';
+export 'data_handler.dart';
 export 'progress_callback.dart';
 export 'aes_core.dart';
 export 'memory_file_system.dart';
